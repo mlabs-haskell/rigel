7dc08834c48c0181a9714e47a86909a9d76175e9
Farseen <oss@itsfarseen.anonaddy.me>
2024-10-14 19:15:19 +0530

    wikipedia_parser: Fix bug due to utf8 length != bytes length
    IndexedFlatFile.get() was returning incorrect data because if a file is opened in text mode, seek operates based on UTF-8 codepoints and not bytes.
    Changed the class so that:
    * The file is opened in binary mode.
    * The responsibility of opening the file with the correct mode is moved back to the IndexedFlatFile class.
      I was accepting Iterable instead of a file so that the caller can decide to wrap the file in tqdm if needed.
      Now we accept a filename and show_progress parameter. We wrap the file in tqdm if show_progress == True.

--- wikipedia_parser/__init__.py
+++ wikipedia_parser/__init__.py
@@ -6,18 +6,29 @@ utility.
 import os
 from typing import Iterable, TextIO
 
+import tqdm
+
+
 class IndexedFlatFile:
-    def __init__(self, index_file: Iterable[str], data_file: TextIO):
+    def __init__(
+        self,
+        index_filename: str,
+        data_filename: str,
+        *,
+        show_progress: bool = False,
+    ):
         """Open the index file, read it into memory and return an IndexedFlatFile."""
+        index_file = open(index_filename)
+        data_file = open(data_filename, "rb")
         file_len = os.fstat(data_file.fileno()).st_size
+        if show_progress:
+            index_file = tqdm.tqdm(index_file, desc="Reading index file")
         self.index_map = self.build_index(index_file, file_len)
         self.data_file = data_file
 
     @classmethod
     def build_index(
-        cls,
-        index_file: Iterable[str],
-        file_len: int
+        cls, index_file: Iterable[str], file_len: int
     ) -> dict[str, tuple[int, int]]:
         index_map = {}
         last_key = None
@@ -37,13 +48,22 @@ class IndexedFlatFile:
         return index_map
 
     def get(self, article_title: str) -> str:
-        """Get the JSON string containing a given article. The JSON structure is:
+        """Get the JSON string containing a given article.
+        The JSON structure is:
         article_schema = {
             "section_name": string (at the top level, this is the article name),
             "text": string (the main body of this section of the article),
             "children": [article_scema]
         }
+        links_schema = {
+            "title": string (article title),
+            "links": [{
+                "target": string (article canonical name used in URLs),
+                "label": string (the text of the link)
+            }]
+        }
         """
         start, end = self.index_map[article_title]
-        self.data_file.seek(start)
-        return self.data_file.read(end - start + 1)
+        self.data_file.seek(start, os.SEEK_SET)
+        bytes = self.data_file.read(end - start + 1)
+        return bytes.decode("utf-8")
