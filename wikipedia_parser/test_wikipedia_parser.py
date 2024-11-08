4424ba86e16235b0787bb9936172fba5a5528365
Farseen <oss@itsfarseen.anonaddy.me>
2024-11-08 16:15:59 +0530

    wikipedia_parser: update tests to use pytest

---
+++ wikipedia_parser/test_wikipedia_parser.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import tempfile
+
+from . import IndexedFlatFile
+
+
+def test_wikipedia_parser():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        p = Path(temp_dir)
+        index_file = p / "index.txt"
+        data_file = p / "data.txt"
+
+        with open(index_file, "wt") as f:
+            f.write("0: Apple\n")
+            f.write("10: Boy\n")
+            f.write("20: Cat\n")
+
+        with open(data_file, "wt") as f:
+            f.write("0123456789ABCDEFGHIJQWERT")
+
+        indexed_file = IndexedFlatFile(str(index_file), str(data_file))
+
+        value = indexed_file.get("Apple")
+        assert value == "0123456789", value
+        value = indexed_file.get("Boy")
+        assert value == "ABCDEFGHIJ", value
+        value = indexed_file.get("Cat")
+        assert value == "QWERT", value
